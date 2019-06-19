d01a56996c58e7ef14edb8b10bc83e5b694aefba
jeffrey_goh <jeffrey_goh@tech.gov.sg>
2019-06-20 07:43:12 +0800

    starting on selenium script

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Selenium
+Currently learning Selenium for the purpose of backend api.
---
+++ boolean.py
@@ -0,0 +1,19 @@
+"""
+Examples to show how boolean works in python
+"""
+
+A = True
+B = False
+
+print(A, B)
+
+print("**************************")
+print(bool(0))
+print(bool(1))
+print(bool(2))
+
+C = ""
+print(bool(C))
+
+C = "Some Value"
+print(bool(C))
