f20ec664a80feac22302259be71e79f2ad05b673
jeffrey_goh <jeffrey_goh@tech.gov.sg>
2019-06-22 14:10:29 +0800

    finished with finding elements

--- find_element.py
+++ find_element.py
@@ -5,8 +5,10 @@ layers are located at /opt/ directory.
 import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementNotVisibleException
+from selenium.common.exceptions import WebDriverException
 
 
 def handler(event, context):
@@ -23,7 +25,9 @@ def handler(event, context):
             "id name": sele.element_govtech_id_name(),
             "xpath css": sele.element_govtech_xpath_css(),
             "link text": sele.element_govtech_link_text(),
-            "class tag": sele.element_govtech_class_tag()
+            "class tag": sele.element_govtech_class_tag(),
+            "by class": sele.element_govtech_by_class(),
+            "element list": sele.govtech_list_of_elements()
         }
     except KeyboardInterrupt:
         obj = {"error": KeyboardInterrupt}
@@ -69,11 +73,14 @@ class Sele:
             print("We found an element by name='viewport'")
             body = driver.find_element_by_xpath("//div[@id='main-content']").text
             print(body)
-            driver.quit()
         except NoSuchElementException:
-            body = NoSuchElementException
+            raise NoSuchElementException
         except ElementNotVisibleException:
-            body = ElementNotVisibleException
+            raise ElementNotVisibleException
+        except WebDriverException:
+            raise WebDriverException
+        finally:
+            driver.quit()
         return body
 
     def element_govtech_xpath_css(self):
@@ -90,11 +97,14 @@ class Sele:
             print("We found an element by css='#search-activate'")
             body = driver.find_element_by_xpath("//div[@id='main-content']").text
             print(body)
-            driver.quit()
         except NoSuchElementException:
-            body = NoSuchElementException
+            raise NoSuchElementException
         except ElementNotVisibleException:
-            body = ElementNotVisibleException
+            raise ElementNotVisibleException
+        except WebDriverException:
+            raise WebDriverException
+        finally:
+            driver.quit()
         return body
 
     def element_govtech_link_text(self):
@@ -111,11 +121,14 @@ class Sele:
             print("We found an element by partial_link_text='Digital'")
             body = driver.find_element_by_xpath("//div[@id='main-content']").text
             print(body)
-            driver.quit()
         except NoSuchElementException:
-            body = NoSuchElementException
+            raise NoSuchElementException
         except ElementNotVisibleException:
-            body = ElementNotVisibleException
+            raise ElementNotVisibleException
+        except WebDriverException:
+            raise WebDriverException
+        finally:
+            driver.quit()
         return body
 
     def element_govtech_class_tag(self):
@@ -134,11 +147,65 @@ class Sele:
             print("We found an element by tag_name=" + key_text.text)
             body = driver.find_element_by_xpath("//div[@id='main-content']").text
             print(body)
-            driver.quit()
         except NoSuchElementException:
-            body = NoSuchElementException
+            raise NoSuchElementException
         except ElementNotVisibleException:
-            body = ElementNotVisibleException
+            raise ElementNotVisibleException
+        except WebDriverException:
+            raise WebDriverException
+        finally:
+            driver.quit()
+        return body
+
+    def element_govtech_by_class(self):
+        """
+        :return: search result from govtech website
+        """
+        driver = self.browser('main')
+        base_url = "https://www.tech.gov.sg/media/"
+        driver.get(base_url)
+        try:
+            # noinspection PyArgumentEqualDefault
+            driver.find_element(By.ID, "2008")
+            print("We found an element by id='2008'")
+            driver.find_element(By.XPATH, "//input[@id='search-box']")
+            print("We found an element by xpath='//input[@id='search-box']'")
+            driver.find_element(By.LINK_TEXT, "Overview")
+            print("We found an element by link_text='Overview'")
+            body = driver.find_element_by_xpath("//div[@id='main-content']").text
+            print(body)
+        except NoSuchElementException:
+            raise NoSuchElementException
+        except ElementNotVisibleException:
+            raise ElementNotVisibleException
+        except WebDriverException:
+            raise WebDriverException
+        finally:
+            driver.quit()
+        return body
+
+    def govtech_list_of_elements(self):
+        """
+        :return: search result from govtech website
+        """
+        driver = self.browser('main')
+        base_url = "https://www.tech.gov.sg/contact-us/"
+        driver.get(base_url)
+        try:
+            class_list = driver.find_elements_by_class_name("row")
+            print("ClassName -> Size of the list is: " + str(len(class_list)))
+            tag_list = driver.find_elements_by_tag_name("div")
+            print("TagName -> Size of the list is: " + str(len(tag_list)))
+            body = driver.find_element_by_xpath("//div[@id='main-content']").text
+            print(body)
+        except NoSuchElementException:
+            raise NoSuchElementException
+        except ElementNotVisibleException:
+            raise ElementNotVisibleException
+        except WebDriverException:
+            raise WebDriverException
+        finally:
+            driver.quit()
         return body
 
 
